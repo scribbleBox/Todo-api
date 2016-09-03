4bd4a5c3d393454ea528fe2c6c0bf7eeb85ee37d
Scribble Box <dave.hilborn@gmail.com>
2016-09-03 19:52:54 +0100

    Added completed filter

--- server.js
+++ server.js
@@ -15,20 +15,22 @@ app.get('/', function (req, res) {
 
 // GET /todos
 app.get('/todos', function (req, res) {
-  res.json(todos);
+  var queryParams = req.query;
+  var filteredTodos = todos;
+
+  if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+    filteredTodos = _.where(filteredTodos, {completed: true});
+  } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+    filteredTodos = _.where(filteredTodos, {completed: false});
+  }
+
+  res.json(filteredTodos);
 });
 
 // GET /todo/:id
 app.get('/todos/:id', function (req, res) {
   var todoId = parseInt(req.params.id, 10);
   var matchedTodo = _.findWhere(todos, {id: todoId});
-  // var matchedTodo;
-
-  // todos.forEach(function (todo) {
-  //   if(todoId === todo.id) {
-  //     matchedTodo = todo;
-  //   }
-  // });
 
   if (matchedTodo) {
     res.json(matchedTodo);
